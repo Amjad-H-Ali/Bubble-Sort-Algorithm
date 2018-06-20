48ebb2e1ebfb095a757ea1d12eef28f3197c9a14
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-20 02:39:52 -0500

    Created swapper function that logs index of numbers in array that need to be swapped

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,31 @@
 console.log("Bubble Sort");
 
+const swapper = (index1, index2, array) => {
+
+	console.log("Swapper function", index1, index2);
+}
+
 
 const bubbleSort = (array) => {
 	for (let i = 0, len = array.length; i < len; i ++) {
 		for (let j = i+1, len = array.length; j < len; j ++) {
 			if (array[i] > array[j]) {
 				console.log(`${array[i]} is GREATER than ${array[j]}!`);
+
+				swapper(i, j, array);
 			}
 
-			else if (array[i] === array[j]) {
-				console.log(`${array[i]} is EQUAL to ${array[j]}!`);
-			}
+			// else if (array[i] === array[j]) {
+			// 	console.log(`${array[i]} is EQUAL to ${array[j]}!`);
+			// }
 
-			else {
-				console.log(`${array[i]} is LESS than ${array[j]}!`);
-			};
+			// else {
+			// 	console.log(`${array[i]} is LESS than ${array[j]}!`);
+			// };
 		};
 	};
+
+	return array;
 };
 
 
