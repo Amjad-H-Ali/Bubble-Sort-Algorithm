a3034f8965ddce64642b9d8e17e478c8f6e13fe0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-20 03:57:03 -0500

    Bubble Sort returns sorted array

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,29 @@
 console.log("Bubble Sort");
 
+// Function that swaps two numbers in an array.
 const swapper = (index1, index2, array) => {
-
+	// Store smaller number.
 	const temp = array[index2];
-
+	// Swap smaller number with larger number
 	array[index2] = array[index1];
-
 	array[index1] = temp;
-
-	console.log(array);
-
-}
+};
 
 
 const bubbleSort = (array) => {
+	// Compare each number with all numbers in array
 	for (let i = 0, len = array.length; i < len; i ++) {
 		for (let j = i+1, len = array.length; j < len; j ++) {
+			// If number is greater than proceeding number, swap.
 			if (array[i] > array[j]) {
-				console.log(`${array[i]} is GREATER than ${array[j]}!`);
-
 				swapper(i, j, array);
-			}
-
-			// else if (array[i] === array[j]) {
-			// 	console.log(`${array[i]} is EQUAL to ${array[j]}!`);
-			// }
-
-			// else {
-			// 	console.log(`${array[i]} is LESS than ${array[j]}!`);
-			// };
+			};
 		};
 	};
-
+	// Return sorted array.
 	return array;
 };
 
 
-const numbers = [9,5,3,6,5,7,9,0,1,2];
+const numbers = [6,3,9,0,6,1,15,32,0,0,14,7,5,8];
 bubbleSort(numbers);
