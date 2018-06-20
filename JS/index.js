6bf7caab047fbdd4686d43f74f2d93141aa7ae8d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-19 22:42:11 -0500

    Added Logic so function compares each number with ALL the proceeding numbers in the array via nested loops

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,22 @@ console.log("Bubble Sort");
 
 
 const bubbleSort = (array) => {
-	array.forEach((num, i) => console.log(num > array[i + 1])); 
-}
+	for (let i = 0, len = array.length; i < len; i ++) {
+		for (let j = i+1, len = array.length; j < len; j ++) {
+			if (array[i] > array[j]) {
+				console.log(`${array[i]} is GREATER than ${array[j]}!`);
+			}
+
+			else if (array[i] === array[j]) {
+				console.log(`${array[i]} is EQUAL to ${array[j]}!`);
+			}
+
+			else {
+				console.log(`${array[i]} is LESS than ${array[j]}!`);
+			};
+		};
+	};
+};
 
 
 const numbers = [9,5,3,6,5,7,9,0,1,2];
