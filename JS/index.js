954bcc2b60e72e649c8265521d7d4fba36df14a7
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-19 07:33:38 -0500

    Updated README

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 console.log("Bubble Sort");
 
+
 // Function that swaps two numbers in an array.
 const swapper = (index1, index2, array) => {
 	// Store smaller number.
@@ -26,4 +27,4 @@ const bubbleSort = (array) => {
 
 
 const numbers = [6,3,9,0,6,1,15,32,0,0,14,7,5,8];
-bubbleSort(numbers);
+console.log(bubbleSort(numbers));
