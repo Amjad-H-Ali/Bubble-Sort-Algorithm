117b71a7ca2ab4a650bf1c378d34b5deb9997832
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-19 07:40:48 -0500

    Made code more succinct

--- JS/index.js
+++ JS/index.js
@@ -4,17 +4,20 @@ console.log("Bubble Sort");
 
 const bubbleSort = (array) => {
 	// Compare each number with all numbers in array
-	for (let i = 0, len = array.length; i < len; i ++) {
-		for (let j = 1; j < len; j ++) {
+	for (let i = 0, len = array.length; i < len; i ++) 
+		for (let j = 1; j < len; j ++) 
 			// If number is greater than proceeding number, swap.
-			if (array[i] > array[j]) {
-				swapper(i, j, array);
-			};
-		};
-	};
+			if (array[j-1] > array[j]) {
+
+				let temp = array[j-1]
+
+				array[j-1] = array[j]
+
+				array[j] = temp
+			}
 	// Return sorted array.
 	return array;
-};
+}
 
 
 const numbers = [6,3,9,0,6,1,15,32,0,0,14,7,5,8];
